b31168203360fed565d70bb4cbe0da00e50e8d96
Irina Benediktovich <i.benediktovich@auckland.ac.nz>
2013-09-02 11:18:24 +1200

    format to tabs

--- src/main/java/nz/ac/auckland/jetty/RemoteUserUtils.java
+++ src/main/java/nz/ac/auckland/jetty/RemoteUserUtils.java
@@ -11,86 +11,87 @@ import java.security.Principal;
 import java.util.Arrays;
 
 public class RemoteUserUtils {
-  private static final Logger log = LoggerFactory.getLogger(RemoteUserUtils.class);
-  private static final String USER_HEADER = "X-Forwarded-User";
-  private static final String REMOTEUSER_NAME = "remoteuser.name";
-  private static final String REMOTEUSER_ROLES = "remoteuser.localroles";
-  public static final String ROLE_CHECK_OVERRIDE_CLASS = "remoteuser.rolesource";
-  private static RoleSource roleSource = null;
+	private static final Logger log = LoggerFactory.getLogger(RemoteUserUtils.class);
+	private static final String USER_HEADER = "X-Forwarded-User";
+	private static final String REMOTEUSER_NAME = "remoteuser.name";
+	private static final String REMOTEUSER_ROLES = "remoteuser.localroles";
+	public static final String ROLE_CHECK_OVERRIDE_CLASS = "remoteuser.rolesource";
+	private static RoleSource roleSource = null;
 
-  public static void initialize() {
-    String clazzName = System.getProperty(ROLE_CHECK_OVERRIDE_CLASS);
+	public static void initialize() {
+		String clazzName = System.getProperty(ROLE_CHECK_OVERRIDE_CLASS);
 
-    if (clazzName != null) {
-      try {
-        Class clazz = Class.forName(clazzName);
-        Object ru = clazz.newInstance();
-        if (ru instanceof RoleSource) {
-          roleSource = (RoleSource)ru;
-        } } catch (Exception ex) {
-        log.error("Unable to find role class " + clazzName, ex);
-      }
-    }
-  }
+		if (clazzName != null) {
+			try {
+				Class clazz = Class.forName(clazzName);
+				Object ru = clazz.newInstance();
+				if (ru instanceof RoleSource) {
+					roleSource = (RoleSource) ru;
+				}
+			} catch (Exception ex) {
+				log.error("Unable to find role class " + clazzName, ex);
+			}
+		}
+	}
 
-  public static Authentication ensureRemoteUserSetIfIncluded(Request request) {
-    String remoteUser = request.getHeader(USER_HEADER);
+	public static Authentication ensureRemoteUserSetIfIncluded(Request request) {
+		String remoteUser = request.getHeader(USER_HEADER);
 
-    // deal with the case where Apache sends through "" as the remote user
-    if (remoteUser != null && remoteUser.trim().length() == 0)
-      remoteUser = null;
+		// deal with the case where Apache sends through "" as the remote user
+		if (remoteUser != null && remoteUser.trim().length() == 0)
+			remoteUser = null;
 
-    if (remoteUser == null) {
-      remoteUser = System.getProperty(REMOTEUSER_NAME);
-    }
+		if (remoteUser == null) {
+			remoteUser = System.getProperty(REMOTEUSER_NAME);
+		}
 
-	  RemoteUserAuthenticationUser user = null;
+		RemoteUserAuthenticationUser user = null;
 
-    if (remoteUser != null) {
-	    user = new RemoteUserAuthenticationUser(remoteUser, request);
+		if (remoteUser != null) {
+			user = new RemoteUserAuthenticationUser(remoteUser, request);
 
-	    if (roleSource != null) { // if we have a role source, force getting it
-		    user.getIdentity().setRoles(roleSource.getRoles(remoteUser, request));
-	    }
-    }
+			if (roleSource != null) { // if we have a role source, force getting it
+				user.getIdentity().setRoles(roleSource.getRoles(remoteUser, request));
+			}
+		}
 
-	  return user;
-  }
+		return user;
+	}
 
-  public static boolean isRoleOverrideSet() {
-    return System.getProperty(REMOTEUSER_ROLES) != null;
-  }
+	public static boolean isRoleOverrideSet() {
+		return System.getProperty(REMOTEUSER_ROLES) != null;
+	}
 
-  public static boolean inRole(String role) {
-    String rSplit = System.getProperty(REMOTEUSER_ROLES);
+	public static boolean inRole(String role) {
+		String rSplit = System.getProperty(REMOTEUSER_ROLES);
 
-    if (rSplit != null) {
-      String[] roles = rSplit.split(",");
-      return Arrays.asList(roles).contains(role);
-    } else
-      return false;
-  }
+		if (rSplit != null) {
+			String[] roles = rSplit.split(",");
+			return Arrays.asList(roles).contains(role);
+		} else
+			return false;
+	}
 
-  public static boolean isUserInRole(RemoteUserIdentity remoteUserIdentity, String role, UserIdentity.Scope scope, Request request) {
-    if (isRoleOverrideSet()) {
-      log.trace("isUserInRole: using overridden roles");
-      return inRole(role);
-    } else {
-      if (remoteUserIdentity != null && remoteUserIdentity.getUserPrincipal().getName() != null && roleSource != null) {
-        log.trace("isUserInRole: asking for roles from roleSource");
+	public static boolean isUserInRole(RemoteUserIdentity remoteUserIdentity, String role, UserIdentity.Scope scope, Request request) {
+		if (isRoleOverrideSet()) {
+			log.trace("isUserInRole: using overridden roles");
+			return inRole(role);
+		} else {
+			if (remoteUserIdentity != null && remoteUserIdentity.getUserPrincipal().getName() != null && roleSource != null) {
+				log.trace("isUserInRole: asking for roles from roleSource");
 
-        if (remoteUserIdentity.getRoles() == null)
-          remoteUserIdentity.setRoles(roleSource.getRoles(remoteUserIdentity.getUserPrincipal().getName(), request));
+				if (remoteUserIdentity.getRoles() == null)
+					remoteUserIdentity.setRoles(roleSource.getRoles(remoteUserIdentity.getUserPrincipal().getName(), request));
 
-	      if (remoteUserIdentity.getRoles() == null) {
-		      return false;
-	      } else {
-		      return remoteUserIdentity.getRoles().contains(role);
-	      }
-      } else {
-        log.trace("isUserInRole: unknown user or user principal not understood");
-        return false;
-      }
-    }
-  }
+				if (remoteUserIdentity.getRoles() == null) {
+					return false;
+				} else {
+					return remoteUserIdentity.getRoles().contains(role);
+				}
+			} else {
+				log.trace("isUserInRole: unknown user or user principal not understood");
+				return false;
+			}
+		}
+	}
 }
